8989c125248a5bb0d98e49d1cb3ead7adcb1e908
AlamgeerRao <105811261+AlamgeerRao@users.noreply.github.com>
2026-06-04 19:21:13 +0100

    Create meals.js

---
+++ src/data/meals.js
@@ -0,0 +1,26 @@
+export const meals = [
+  {
+    id: "meal1",
+    name: "Chicken Curry",
+    description: "Home-style chicken curry with rice",
+    price: 8.99,
+    image: "/images/chicken-curry.jpg",
+    category: "mains"
+  },
+  {
+    id: "meal2",
+    name: "Vegetable Biryani",
+    description: "Mixed vegetable biryani (vegan)",
+    price: 7.99,
+    image: "/images/veg-biryani.jpg",
+    category: "vegetarian"
+  },
+  {
+    id: "meal3",
+    name: "Family Dinner Box",
+    description: "Meal for 4 people",
+    price: 18.99,
+    image: "/images/family-box.jpg",
+    category: "specials"
+  }
+];
